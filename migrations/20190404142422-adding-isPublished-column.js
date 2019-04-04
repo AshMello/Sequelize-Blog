51c4f00955629b960e4e4d6abf94a7d7e1ee81ef
Ashley <47507695+AshMello@users.noreply.github.com>
2019-04-04 09:32:54 -0500

    migration added

---
+++ migrations/20190404142422-adding-isPublished-column.js
@@ -0,0 +1,20 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    queryInterface.addColumn(
+      'Posts', 
+      'isPublished',{ 
+        type: Sequelize.BOOLEAN 
+      }
+    )
+  },
+
+  down: (queryInterface, Sequelize) => {
+    queryInterface.removeColumn(
+      'Posts',
+      'isPublished'
+    )
+
+  }
+};
